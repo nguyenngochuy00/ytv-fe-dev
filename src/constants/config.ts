f1f4b5c82e626d2910d6ad780a8382c889fc1af2
nguyenngochuy00 <ngochuy25120@gmail.com>
2025-12-30 15:09:08 +0700

    Update url odoo

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,6 @@
 export const ODOO_CONFIG = {
-  URL: process.env.NEXT_PUBLIC_ODOO_URL || "http://localhost:8069",
+  URL:
+    process.env.NEXT_PUBLIC_ODOO_URL || "https://c29b92f1c760.ngrok-free.app/",
   DB: process.env.NEXT_PUBLIC_ODOO_DB || "admin_251217",
 } as const;
 
